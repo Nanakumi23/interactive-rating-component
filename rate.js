99bf1dd70e93288f02bbbc31cd458a108a11d08a
Nana kumi <nanadude11@gmail.com>
2024-09-14 20:11:30 -0400

    js

---
+++ rate.js
@@ -0,0 +1,8 @@
+let button = document.getElementById('btn'); 
+let message = document.getElementById('thankYou')
+let action = ()=>{
+    message.style.display='none';
+ 
+}
+
+button.addEventListener('click', action)
