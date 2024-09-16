2a10a211ebacc3ce573f8142b472ea8c952039d6
Nana kumi <nanadude11@gmail.com>
2024-09-16 08:16:56 -0400

    finished setting up fumctionality

--- rate.js
+++ rate.js
@@ -4,12 +4,11 @@ let vote = document.getElementById('black');
 let vote2 = document.getElementById('black2'); 
 let vote3 = document.getElementById('black3'); 
 let vote4 = document.getElementById('black4'); 
+let vote5 = document.getElementById('black5'); 
 
 
 let voting = (event)=>{
     let hasVoted = false;
-
-   
       if (!hasVoted) {
         event.target.style.backgroundColor = 'blue';
         
@@ -18,17 +17,29 @@ let voting = (event)=>{
         vote2.removeEventListener('click', voting);
         vote3.removeEventListener('click', voting);
         vote4.removeEventListener('click', voting);
+        vote5.removeEventListener('click', voting);
       }
     };
+let undoColor = ()=>{
+    vote.style.backgroundColor= 'white';
+    vote2.style.backgroundColor= 'white';
+    vote3.style.backgroundColor= 'white';
+    vote4.style.backgroundColor= 'white';
+    vote5.style.backgroundColor= 'white';
 
-// let action = (event)=>{
-//     // Hide thank you message
-//    event.target.style.backgroundColor= 'white'
-    
-// }xs
+    vote.addEventListener('click',voting);
+ vote2.addEventListener('click',voting);
+ vote3.addEventListener('click',voting);
+ vote4.addEventListener('click',voting);
+ vote5.addEventListener('click',voting);
+}
+
+
 
  vote.addEventListener('click',voting);
  vote2.addEventListener('click',voting);
  vote3.addEventListener('click',voting);
  vote4.addEventListener('click',voting);
-// button.addEventListener('click', action);
+ vote5.addEventListener('click',voting);
+
+ button.addEventListener('click', undoColor);
