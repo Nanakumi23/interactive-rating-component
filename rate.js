a0c7d3924421693620b83ea6dfe30f19d64b6c65
Nana kumi <nanadude11@gmail.com>
2024-09-16 07:40:52 -0400

    fixed buttons

--- rate.js
+++ rate.js
@@ -1,8 +1,34 @@
 let button = document.getElementById('btn'); 
 let message = document.getElementById('thankYou')
-let action = ()=>{
-    message.style.display='none';
- 
-}
+let vote = document.getElementById('black'); 
+let vote2 = document.getElementById('black2'); 
+let vote3 = document.getElementById('black3'); 
+let vote4 = document.getElementById('black4'); 
 
-button.addEventListener('click', action)
+
+let voting = (event)=>{
+  let ans;
+    if(event.target.id == vote.id){
+       
+            vote.style.backgroundColor='blue'
+        } 
+
+    else if(event.target.id == vote2.id){
+       vote2.style.backgroundColor='blue'}
+    else if(event.target.id == vote3.id){
+            vote3.style.backgroundColor='blue'}
+    else if(event.target.id == vote4.id){
+            vote4.style.backgroundColor='blue'}
+   
+    }
+// let action = (event)=>{
+//     // Hide thank you message
+//    event.target.style.backgroundColor= 'white'
+    
+// }xs
+
+ vote.addEventListener('click',voting);
+ vote2.addEventListener('click',voting);
+ vote3.addEventListener('click',voting);
+ vote4.addEventListener('click',voting);
+// button.addEventListener('click', action);
